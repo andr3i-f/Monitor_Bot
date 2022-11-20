73ed4332ee4ddf158bbf2855e442f5cc33517a01
Andrei <andrei.florea003@gmail.com>
2022-11-19 16:39:50 -0800

    discord bot and database linked

--- bot/bot.py
+++ bot/bot.py
@@ -1,46 +1,127 @@
 import discord
-import mysql.connector
+from discord.ext import commands
+from datetime import datetime
 import db
 secret = "MTAyMTIyMDQ1Mzc1NjQ1Njk3MA.GX9KPz.cGUUchaSR1ldDQaGgUdQzFtACKQ3PtIz5Meefs"
 # inv https://discord.com/api/oauth2/authorize?client_id=1021220453756456970&permissions=1634504013888&scope=bot
 
 
+membership_role = "1043673145074253875"
+
 
 def main():
     intents = discord.Intents.default()
     intents.message_content = True
-    client = discord.Client(intents=intents)
+    client = commands.Bot(command_prefix='$', intents=intents)
 
     database = db.database()
 
-    @client.event
-    async def on_ready():
-        print(f"We have logged in as {client.user}")
+    
+    @client.command()
+    async def test(ctx, arg):
+        print(ctx.author.id)
+        print(ctx.author.roles)
+        await ctx.send(arg)
+    
+    @client.command()
+    async def check_role(ctx):
+        has_role = ctx.author.get_role(int(membership_role))
+        if has_role:
+            await ctx.send("You have the membership role")
+        elif not has_role:
+            await ctx.send("You do not have the membership role")
 
+    @client.command()
+    async def add_client(ctx):
+        has_role = ctx.author.get_role(int(membership_role))
 
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
+        if has_role:
+            discord_id = str(ctx.author.id)
+            discord_name = str(ctx.author)
+            membership = "Y"
+            created = datetime.now()
+
+            database.add_client(discord_id, discord_name, membership, created)
         
-        if message.content.startswith('$hello'):
-            await message.channel.send('Hello!')
+        elif not has_role:
+            await ctx.send("Cannot add, try $check_role")
+
+    @client.command()
+    async def add_shopify(ctx, arg):
+        key = str(arg)
+        discord_id = str(ctx.author.id)
+        time = datetime.now()
+        result = None
+
+        ID = database.get_ID(discord_id)
+        member = database.get_membership_role(discord_id)
+        if ID == False:
+            await ctx.send("Could not find in database, try $add_client")
+
+        elif ID and member == 'Y':
+            result = database.add_webhook_supreme(ID, key, time)        
         
-        if message.content.startswith('!add_key '):
-            key = str(message.clean_content[9:])
-            author = str(message.author.id)
-            name = str(message.author)
+        if result:
+            await ctx.send("Added webhook into database")
+        elif not result:
+            await ctx.send("Could not add webhook into database")
+    
+    async def add_supreme(ctx, arg):
+        key = str(arg)
+        discord_id = str(ctx.author.id)
+        time = datetime.now()
+        result = None
 
-            #print(key)
-            #print(author)
-            #print(name)
+        ID = database.get_ID(discord_id)
+        member = database.get_membership_role(discord_id)
+        if ID == False:
+            await ctx.send("Could not find in database, try $add_client")
+        
+        elif ID and member == 'Y':
+            result = database.add_webhook_supreme(ID, key, time)
 
-            t = database.add_webhook(author, name, key)
-            database.commit()
-            #print(t)
+        if result:
+            await ctx.send("Added webhook into database")
+        elif not result:
+            await ctx.send("Could not add webhook into database")
 
-            database.show_users()
-            await message.channel.send(message.clean_content[8:])
+    async def add_footlocker(ctx, arg):
+        key = str(arg)
+        discord_id = str(ctx.author.id)
+        time = datetime.now()
+        result = None
+
+        ID = database.get_ID(discord_id)
+        member = database.get_membership_role(discord_id)
+        if ID == False:
+            await ctx.send("Could not find in database, try $add_client")
+        
+        elif ID and member == 'Y':
+            result = database.add_webhook_supreme(ID, key, time)
+
+        if result:
+            await ctx.send("Added webhook into database")
+        elif not result:
+            await ctx.send("Could not add webhook into database")
+
+    async def add_nike(ctx, arg):
+        key = str(arg)
+        discord_id = str(ctx.author.id)
+        time = datetime.now()
+        result = None
+
+        ID = database.get_ID(discord_id)
+        member = database.get_membership_role(discord_id)
+        if ID == False:
+            await ctx.send("Could not find in database, try $add_client")
+
+        elif ID and member == 'Y':
+            result = database.add_webhook_supreme(ID, key, time) 
+
+        if result:
+            await ctx.send("Added webhook into database")
+        elif not result:
+            await ctx.send("Could not add webhook into database")             
 
     client.run(secret)
 
--- bot/db.py
+++ bot/db.py
@@ -14,8 +14,11 @@ from datetime import datetime
 """
     #query1 = "CREATE TABLE Clients (id int PRIMARY KEY AUTO_INCREMENT, user_id VARCHAR(50), name VARCHAR(50), webhook VARCHAR(150))"
     
+
+tables = ['shopify', 'footlocker', 'supreme', 'nike']
+
 class database:
-    def __init__(self, host="localhost", user="root", passwd="root", database="testdatabase"):
+    def __init__(self, host="localhost", user="root", passwd="root", database="A1Monitors"):
         self.host = host
         self.user = user
         self.passwd = passwd
@@ -32,21 +35,101 @@ class database:
     
     def commit(self):
         self.db.commit()
-    
-    def add_webhook(self, input_user_id, input_name, input_webhook):
-        # This will check, and if there is no user for the webhook then it will add it, if there is a user with this webhook it will change it
-        query = "INSERT INTO Clients (user_id, name, webhook) VALUES (%s, %s, %s)"
-        val = (input_user_id, input_name, input_webhook)
+
+    def add_client(self, discord_id, discord_name, membership, created):
+        # This will add in a client if they have the membership role
+        query = "INSERT INTO Clients (DiscordID, DiscordName, Membership, Created) VALUES (%s, %s, %s, %s)"
+        val = (discord_id, discord_name, membership, created)
         self.mycursor.execute(query, val)
+
+        self.commit()
+        
+        pass
+    
+    def add_webhook_shopify(self, ID, webhook, created):
+        # This will check, and if there is no user for the webhook then it will add it, if there is a user with this webhook it will change it
+        query = "INSERT INTO Shopify (ID, webhook, Created) VALUES (%s, %s, %s)"
+        val = (ID, webhook, created)
+        self.mycursor.execute(query, val)
+
+        self.commit()
+
         return True
     
-    def remove_webhook(self, ):
+    def add_webhook_supreme(self, ID, webhook, created):
+        query = "INSERT INTO Supreme (ID, webhook, Created) VALUES (%s, %s, %s)"
+        val = (ID, webhook, created)
+        self.mycursor.execute(query, val)
+
+        self.commit()
+        
+        return True      
+
+    def add_webhook_footlocker(self, ID, webhook, created):
+        query = "INSERT INTO Footlocker (ID, webhook, Created) VALUES (%s, %s, %s)"
+        val = (ID, webhook, created)
+        self.mycursor.execute(query, val)
+
+        self.commit()
+
+        return True 
+
+    def add_webhook_nike(self, ID, webhook, created):
+        query = "INSERT INTO Nike (ID, webhook, Created) VALUES (%s, %s, %s)"
+        val = (ID, webhook, created)
+        self.mycursor.execute(query, val)
+
+        self.commit()
+
+        return True 
+    
+    def remove_webhook(self):
         # This will remove a webhook if the user does not want the webhook anymore
         pass
 
-    def remove_user(self,):
+    def remove_user(self):
         # This will remove a user if the user does not have membership anymore
         pass
+
+    def get_membership_role(self, discordID):
+        # This will return true or false depending if the user has a current membership in client database
+        query = f"SELECT Membership FROM Clients WHERE DiscordID={discordID}"
+        self.mycursor.execute(query)
+
+        try:
+            member = self.mycursor.fetchone()[0]
+        except TypeError:
+            member = None
+
+        if member == None:
+            print('Member not found')
+            return None
+        elif member:
+            return member
+
+
+    def get_ID(self, discordID):
+        # This will get the actual ID (not discord) from the user if they have membership
+        query = f"SELECT ID FROM Clients WHERE DiscordID={discordID}"
+        self.mycursor.execute(query)
+
+        try:
+            id = self.mycursor.fetchone()[0]
+        except TypeError:
+            id = False
+
+        if not id:
+            return False
+        elif id:
+            return id
+
+    def add_user(self):
+        # This is for testing purposes
+        query = "INSERT INTO Clients (DiscordID, DiscordName, Membership, Created) VALUES (%s, %s, %s, %s)"
+        val = ("12356", "Andrei", "Y", datetime.now())
+
+        self.mycursor.execute(query, val)
+        self.commit()
 
     def show_users(self):
         # This will show the current users
@@ -55,3 +138,44 @@ class database:
 
         for x in self.mycursor:
             print(x)
+    
+    def show_table_contents(self, table):
+        # This will show contents of a table
+        query = f"SELECT * FROM {table}"
+        self.mycursor.execute(query)
+
+        for x in self.mycursor:
+            print(x)
+    
+    def show_tables(self):
+        # This will show current tables
+        self.mycursor.execute("Show tables;")
+        myresult = self.mycursor.fetchall()
+
+        for x in myresult:
+            print(x)
+
+    def describe_table(self, table):
+        # This will describe the table given
+        self.mycursor.execute(f"DESCRIBE {table}")
+
+        for x in self.mycursor:
+            print(x)
+    
+    def create_tables(self):
+        # Creates tables, only used once
+        #self.mycursor.execute("CREATE TABLE Clients (DiscordID VARCHAR(100) NOT NULL, DiscordName VARCHAR(100) NOT NULL, Membership ENUM('Y', 'N') NOT NULL, Created datetime, ID int PRIMARY KEY NOT NULL AUTO_INCREMENT)")
+        #self.mycursor.execute("CREATE TABLE Supreme (ID int PRIMARY KEY, FOREIGN KEY(ID) REFERENCES Clients(ID), webhook VARCHAR(250), Created datetime)")
+        pass
+    
+def main():
+    db = database()
+    #db.add_user()
+    db.show_users()
+    #print(db.get_ID('12356'))
+    #db.describe_table('shopify')
+    #db.show_table_contents('shopify')
+    db.get_membership_role('121820049940938754')
+
+if __name__ == "__main__":
+    main()
