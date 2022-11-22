d7c5d450e37c448301a926408460c644d89b475e
Andrei <andrei.florea003@gmail.com>
2022-11-21 19:21:35 -0800

    Added modals for webhook input

--- bot/bot_form.py
+++ bot/bot_form.py
@@ -11,12 +11,25 @@ def main():
 
     @tree.command(name="ping", description="Will pong", guild=discord.Object(id=guild_id))
     async def ping(interaction):
-        await interaction.response.send_message("pong")
+        await interaction.response.send_message("pong", ephemeral=True)
+
     
-    @tree.command(name="add_webhook", description="Add webhook", guild=discord.Object(id=guild_id))
-    async def add_webhook(interaction):
-        await interaction.response.send
-    
+    @tree.command(name="shopify_webhook", description="Add your shopify webhook", guild=discord.Object(id=guild_id))
+    async def shopify_webhook(interaction):
+        await interaction.response.send_modal(webhook_form_shopify())
+
+    @tree.command(name="supreme_webhook", description="Add your shopify webhook", guild=discord.Object(id=guild_id))
+    async def shopify_webhook(interaction):
+        await interaction.response.send_modal(webhook_form_supreme())
+
+    @tree.command(name="footlocker_webhook", description="Add your shopify webhook", guild=discord.Object(id=guild_id))
+    async def shopify_webhook(interaction):
+        await interaction.response.send_modal(webhook_form_footlocker())
+
+    @tree.command(name="nike_webhook", description="Add your shopify webhook", guild=discord.Object(id=guild_id))
+    async def shopify_webhook(interaction):
+        await interaction.response.send_modal(webhook_form_nike())
+
     @client.event
     async def on_ready():
         await tree.sync(guild=discord.Object(id=guild_id))
@@ -25,6 +38,38 @@ def main():
 
     client.run(secret)
 
+class webhook_form_shopify(discord.ui.Modal, title="Shopify Webhook Input"):   # SHOPIFY WEBHOOK INPUT
+    webhook = discord.ui.TextInput(label="WEBHOOK INPUT", style=discord.TextStyle.paragraph)
+    
+
+    async def on_submit(self, interaction: discord.Interaction):
+        # When person presses submit, this is where I try to add their webhook
+        await interaction.response.send_message(f"Submission entered", ephemeral=True)
+
+class webhook_form_supreme(discord.ui.Modal, title="Supreme Webhook Input"):  # SUPREME WEBHOOK INPUT
+    webhook = discord.ui.TextInput(label="WEBHOOK INPUT", style=discord.TextStyle.paragraph)
+    
+
+    async def on_submit(self, interaction: discord.Interaction):
+        # When person presses submit, this is where I try to add their webhook
+        await interaction.response.send_message(f"Submission entered", ephemeral=True)
+
+class webhook_form_footlocker(discord.ui.Modal, title="Footlocker Webhook Input"):  # FOOTLOCKER WEBHOOK INPUT
+    webhook = discord.ui.TextInput(label="WEBHOOK INPUT", style=discord.TextStyle.paragraph)
+    
+
+    async def on_submit(self, interaction: discord.Interaction):
+        # When person presses submit, this is where I try to add their webhook
+        await interaction.response.send_message(f"Submission entered", ephemeral=True)
+
+
+class webhook_form_nike(discord.ui.Modal, title="Nike Webhook Input"):  # NIKE WEBHOOK INPUT
+    webhook = discord.ui.TextInput(label="WEBHOOK INPUT", style=discord.TextStyle.paragraph)
+    
+
+    async def on_submit(self, interaction: discord.Interaction):
+        # When person presses submit, this is where I try to add their webhook
+        await interaction.response.send_message(f"Submission entered", ephemeral=True)
 
 if __name__ == "__main__":
     main()
